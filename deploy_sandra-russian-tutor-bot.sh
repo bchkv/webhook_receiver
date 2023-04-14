e3147ed94604afc051194a198115392bea55e371
hyper <hyperskill@hyperskill.org>
2023-04-14 16:54:09 -0600

    Working version for SandraRussianTutorBot

--- deploy_sandra-russian-tutor-bot.sh
+++ deploy_sandra-russian-tutor-bot.sh
@@ -4,5 +4,5 @@ git fetch --all
 git reset --hard origin/main
 git pull origin main
 kill $(pgrep -f bot.py)
-nohup python3 bot.py &
+/usr/bin/nohup python3 bot.py >mylogfile.log 2>&1 &
 echo "Bot script updated and restarted successfully."
